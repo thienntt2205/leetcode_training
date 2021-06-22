f7767ab33c4e82a144998e0d4957edc1925283bf
Thien <40974477+thienntt2205@users.noreply.github.com>
2021-06-22 21:05:57 +0700

    add 121,122,125 leetcode problem.

---
+++ src/main/java/leetcode/No122_BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
+public class No122_BestTimeToBuyAndSellStockII {
+    public int maxProfit(int[] prices) {
+        int i = 0, buy, sell, profit = 0, N = prices.length - 1;
+        while (i < N) {
+            while (i < N && prices[i + 1] <= prices[i]) i++;
+            buy = prices[i];
+
+            while (i < N && prices[i + 1] > prices[i]) i++;
+            sell = prices[i];
+
+            profit += sell - buy;
+        }
+        return profit;
+    }
+}
