88d0a0f799f47f7f1e2a29dcad6ce50a79d5b8c7
Thien <40974477+thienntt2205@users.noreply.github.com>
2021-07-05 20:21:13 +0700

    Add 326 and Test

---
+++ src/test/java/leetcode/No326_PowerOfThreeTest.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class No326_PowerOfThreeTest {
+
+    @Test
+    public void isPowerOfThree() {
+        No326_PowerOfThree check = new No326_PowerOfThree();
+        assertFalse(check.isPowerOfThree(28));
+    }
+}
