d479462a4a3df2cd8f0f12f1ae6f550ad432c26d
Thien <40974477+thienntt2205@users.noreply.github.com>
2021-07-08 00:01:31 +0700

    Add 387 and test

---
+++ src/test/java/leetcode/No387_FirstUniqueCharacterInAStringTest.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class No387_FirstUniqueCharacterInAStringTest {
+
+    @Test
+    public void firstUniqChar() {
+        No387_FirstUniqueCharacterInAString check = new No387_FirstUniqueCharacterInAString();
+        assertEquals(0, check.firstUniqChar("leetcode"));
+    }
+}
