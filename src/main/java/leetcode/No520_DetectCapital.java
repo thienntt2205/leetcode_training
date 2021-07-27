2ed2fcfbb931a8d852c791b2fab516b6d6be2bbc
thien <thien.ntt2205@gmail.com>
2021-07-27 17:11:31 +0700

    Renew Project

--- src/main/java/leetcode/No520_DetectCapital.java
+++
@@ -1,8 +0,0 @@
-//https://leetcode.com/problems/detect-capital/
-package leetcode;
-
-public class No520_DetectCapital {
-    public int findLUSlength(String a, String b) {
-        return a.equals(b) ? -1 : Math.max(a.length(), b.length());
-    }
-}
--- src/main/java/leetcode/No69_Sqrt_x.java
+++
@@ -1,8 +0,0 @@
-package leetcode;
-
-public class No69_Sqrt_x {
-    public int mySqrt(int x) {
-
-        return (int) Math.pow(x, 0.5);
-    }
-}
